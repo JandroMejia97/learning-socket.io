e27bf9b71224aab3c775b2fd6ef83dd8cb268cd1
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-07-26 15:08:43 -0300

    Adding initial config

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+const port = process.env.SERVER_PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.info(`Server listening on port ${port}`);
+});
